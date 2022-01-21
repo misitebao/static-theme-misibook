770be69e6d750190b73707c877366608e30b4519
misitebao <i@misitebao.com>
2022-01-22 01:57:08 +0800

    feat: set template compilation options

--- src/about.js
+++ src/about.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 
 createApp({
+  // Setting the Vue template file rendering flag is mainly to avoid conflicts with other rendering engine tags
+  // 设置Vue模板文件渲染标记符,主要是为了避免和其他渲染引擎标记冲突
+  compilerOptions: {
+    delimiters: ["{[", "]}"],
+    comments: true,
+  },
   setup() {
     let name = "zhangsan";
     return {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import { createApp } from "vue/dist/vue.esm-bundler.js";
 import Hello from "@/components/HelloWorld.vue";
 
 createApp({
+  // Setting the Vue template file rendering flag is mainly to avoid conflicts with other rendering engine tags
+  // 设置Vue模板文件渲染标记符,主要是为了避免和其他渲染引擎标记冲突
+  compilerOptions: {
+    delimiters: ["{[", "]}"],
+    comments: true,
+  },
   components: {
     Hello,
   },
